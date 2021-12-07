1e8166d179ccee6d796b3cccc5e3d05a4b9fab50
Axel KUHNEL <axelkuhnel94@gmail.com>
2021-12-07 10:41:16 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+function imgSlide(empty){
+    document.querySelector('.jordan').src = empty;
+}
+
+function bgcolorChange(color){
+    const change = document.querySelector('.home');
+    change.style.background = color;
+}
